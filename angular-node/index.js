b5c14933429d4b8996fccd9be4594473e9552d94
arkpery <thomas.simoes.dev@gmail.com>
2021-06-16 12:22:55 +0200

    node server

--- angular-node/index.js
+++ angular-node/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
+const fs = require("fs");
 
 http.createServer((req, res) => {
-    res.end(`/home/node/app/index.html`);
+    res.end(fs.readFileSync(`/home/node/app/index.html`));
 }).listen(80);
